6b834c83512b275a1653470f26d452fda65eccaa
Ben Oliver <benjamin.e.oliver@gmail.com>
2021-12-30 17:43:12 +0100

    filter specs

--- client/src/components/Specs.tsx
+++ client/src/components/Specs.tsx
@@ -1,11 +1,36 @@
-import React, { useEffect, useState } from "react"
+import React, { ChangeEvent, useEffect, useState } from "react"
 import { Repl } from "../lib/repl/repl"
 import type { Symbol } from "../lib/repl/clojure";
 
 interface Props { repl: Repl, ns?: string }
 
+
+const filterEntries = (entries : IterableIterator<[string, string[]]>, nsRegex :RegExp , nameRegex : RegExp) => {
+    const xs = Array.from(entries).map(([k,v]) => [k, v.filter(ns => ns.match(nsRegex))]) as [string, string[]][]
+    return xs.filter(([name,v]) => name.match(nameRegex) && v.length > 0)    
+}
+
 const Specs: React.FunctionComponent<Props> = ({ repl }) => {
+
+    const [nsRegex, setNsRegex] = useState({ regex: new RegExp(""), display: "" })
+    const [nameRegex, setNameRegex] = useState({ regex: new RegExp(""), display: "" })
+
+    const handleNsRegexChange = (event: ChangeEvent<HTMLInputElement>) => {
+        try {
+            const regex = new RegExp("^" + event.target.value)
+            setNsRegex({ regex, display: event.target.value })
+        } catch (error) { }
+    }
+
+    const handleNameRegexChange = (event: ChangeEvent<HTMLInputElement>) => {
+        try {
+            const regex = new RegExp("^" + event.target.value)
+            setNameRegex({ regex, display: event.target.value })
+        } catch (error) { }
+    }
+
     const [specs, setSpecs] = useState<{ specs: Array<string>, mappings: Map<string, string[]> }>({ specs: [], mappings: new Map() })
+
     useEffect(() => {
         (async () => {
             repl.allSpecs().then((data) => {
@@ -31,15 +56,30 @@ const Specs: React.FunctionComponent<Props> = ({ repl }) => {
             })
         })()
     }, [repl])
-    return <div>{Array.from(specs.mappings.entries()).map(([name, ns], i) =>
-    (
-        <div key={i}>
-            <h3>{name}</h3>
-            <div>
-                {ns.map((x, i) => <ul key={i}>{x}</ul>)}
-            </div>
+
+    return (
+        <div>
+            <form >
+                <label>
+                    ns pattern:
+                    <input type="text" value={nsRegex.display} onChange={handleNsRegexChange} />
+                </label>
+                <label>
+                    name pattern:
+                    <input type="text" value={nameRegex.display} onChange={handleNameRegexChange} />
+                </label>
+            </form>
+            {filterEntries(specs.mappings.entries(), nsRegex.regex, nameRegex.regex).map(([name, namespaces], i) =>
+            (
+                <div key={i}>
+                    <h3>{name}</h3>
+                    <div>
+                        {namespaces.filter((ns) => ns.match(nsRegex.regex)).map((x, i) => <ul key={i}>{x}</ul>)}
+                    </div>
+                </div>
+            ))}
         </div>
-    ))}</div>
+    )
 }
 
 export default Specs
