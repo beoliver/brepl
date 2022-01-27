d41b1a3d1663b9d001636fc21e7f88da050aa37a
Ben Oliver <benjamin.e.oliver@gmail.com>
2022-01-27 19:49:31 +0100

    indentation formatting

--- prepl.sh
+++ prepl.sh
@@ -5,6 +5,6 @@ set -eo pipefail
 PORT=$1
 
 clj -J-Xverify:none \
-     -J-XX:+TieredCompilation \
-     -J-XX:TieredStopAtLevel=1 \
-     -J-Dclojure.server.repl="{:port ${PORT} :accept clojure.core.server/io-prepl}" \
+    -J-XX:+TieredCompilation \
+    -J-XX:TieredStopAtLevel=1 \
+    -J-Dclojure.server.repl="{:port ${PORT} :accept clojure.core.server/io-prepl}" \
